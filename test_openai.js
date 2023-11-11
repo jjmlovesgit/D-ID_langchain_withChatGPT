f220f26e2185f1e8d4d9d5938d080b738aa90ea7
JJM <47751509+jjmlovesgit@users.noreply.github.com>
2023-11-10 22:33:19 -0500

    Update test_openai.js

--- test_openai.js
+++ test_openai.js
@@ -14,7 +14,7 @@ async function main() {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                model: "gpt-3.5-turbo",
+                model: "model: "gpt-4-1106-preview",
                 messages: [{"role": "user", "content": prompt}],
                 temperature: 0.7
             }),
